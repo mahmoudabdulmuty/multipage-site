a3b5db1fb7722f907b09d56ffb078f08e19467ed
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-18 03:50:32 +0200

    adding useParams Hook

--- src/Pages/Article.js
+++ src/Pages/Article.js
@@ -1,3 +1,17 @@
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
+
 export default function Article() {
-	return <div>Articles Page</div>;
+	const { id } = useParams();
+	const url = 'http://localhost:3000/articles/' + id;
+	const { data: article, error, isPending } = useFetch(url);
+	return (
+		<div>
+			{isPending && <div>Loading ...</div>}
+			{error && { error }}
+			<h2>{article.title}</h2>
+			<h3>by: {article.author}</h3>
+			<p>{article.body}</p>
+		</div>
+	);
 }
